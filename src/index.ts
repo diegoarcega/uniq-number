fb16c14d8ec3725855349db921d57d4a64817e62
Diego Arcega <diegoarcega@gmail.com>
2020-03-25 13:54:30 -0300

    testing bundle size

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,25 @@
-export { createUniqNumberGenerator } from './create-uniq-number-generator'
-export { getUniq } from './get-uniq'
+function* createUniqNumberGenerator(startingNumber: number = 0): Generator<number, undefined, boolean> {
+  let number = startingNumber
+
+  while (true) {
+    yield number++
+  }
+}
+
+const globalGenerator = createUniqNumberGenerator()
+
+
+export function getUniq(startingNumber: number = 0): number | Function {
+  const generator = createUniqNumberGenerator(startingNumber)
+
+  if (startingNumber !== 0) {
+    return function () {
+      return generator.next().value
+    }
+  }
+
+  return globalGenerator.next().value
+}
+
+
+export { createUniqNumberGenerator }
